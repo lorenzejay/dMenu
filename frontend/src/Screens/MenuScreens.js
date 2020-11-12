176f2c21964df1d01389fdfa5a26b99b93fa4201
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-12 10:24:16 -0800

    created a filter by section on menu screens, frontend only

--- frontend/src/Screens/MenuScreens.js
+++ frontend/src/Screens/MenuScreens.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import MenuItemCard from "../Components/MenuItemCard";
 import { useSelector, useDispatch } from "react-redux";
-import { getMenu, getUserMenuDetails } from "../Redux/Actions/userActions";
+import { getMenu } from "../Redux/Actions/userActions";
 import Loader from "../Components/Loader";
 import Message from "../Components/Message";
 
 const MenuScreens = ({ history }) => {
+  const [filterBy, setFilterBy] = useState("All");
+  const [menuItems, setMenuItems] = useState();
   const dispatch = useDispatch();
 
   const userMenu = useSelector((state) => state.userMenu);
@@ -22,16 +24,42 @@ const MenuScreens = ({ history }) => {
     }
   }, [userInfo, success]);
 
+  //filter by
+  useEffect(() => {
+    //create filter
+    if (menu) {
+      const filteredByCategory = menu.filter((item) => item.category === filterBy);
+      if (filterBy === "All") {
+        setMenuItems(menu);
+      } else {
+        setMenuItems(filteredByCategory);
+      }
+    }
+  }, [success, filterBy]);
+  console.log(filterBy, menuItems);
+
   return (
     <div className="menu-section">
       {isLoading && <Loader />}
       {error && <Message variant="danger">{error}</Message>}
-      {menu &&
-        menu.map((item, i) => (
-          <div key={i}>
-            <MenuItemCard item={item} />
-          </div>
-        ))}
+      <div className="menu-section-header">
+        <h1>{userInfo.restaurantName}</h1>
+        <div className="menu-section-options">
+          <p onClick={(e) => setFilterBy(e.target.textContent)}>All</p>
+          <p onClick={(e) => setFilterBy(e.target.textContent)}>Breakfast</p>
+          <p onClick={(e) => setFilterBy(e.target.textContent)}>Lunch</p>
+          <p onClick={(e) => setFilterBy(e.target.textContent)}>Dinner</p>
+          <p onClick={(e) => setFilterBy(e.target.textContent)}>Drinks</p>
+        </div>
+      </div>
+      <div className="menu-section-items">
+        {menuItems &&
+          menuItems.map((item, i) => (
+            <div key={i}>
+              <MenuItemCard item={item} />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
